b05c48666522833c35ebc75d014f94f5adb71d27
abilov.w <109854878+Shirxan@users.noreply.github.com>
2022-09-12 17:55:17 +0300

    Add files via upload

---
+++ 06.09.2022/script.js
@@ -0,0 +1,10 @@
+
+
+function Upper(){
+let email = document.getElementById("email");
+email.value = email.toUpperCase; 
+}
+function Lower(){
+    let text = document.getElementById("text");
+    text.value = text.toLowerCase;
+}
